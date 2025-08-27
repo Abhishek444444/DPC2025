c0b2e9dcff8f12864c464a582dc665e190126fcc
Abhishek444444 <144290584+Abhishek444444@users.noreply.github.com>
2025-08-28 00:32:38 +0530

    Longest Palindromic Substring Solution

---
+++ day13.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd length palindrome (center = single char)
+            int len1 = expandAroundCenter(s, i, i);
+            // Even length palindrome (center = between two chars)
+            int len2 = expandAroundCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                // Update start & end indices
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // length of palindrome
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.longestPalindrome("babad")); // bab or aba
+        System.out.println(sol.longestPalindrome("cbbd"));  // bb
+        System.out.println(sol.longestPalindrome("a"));     // a
+        System.out.println(sol.longestPalindrome("aaaa"));  // aaaa
+        System.out.println(sol.longestPalindrome("abc"));   // a
+    }
+}
